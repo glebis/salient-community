38b2d50155107dc9129737402808ab9344179fda
glebis <glebis@gmail.com>
2026-02-22 23:32:35 +0100

    Update site title, add footer link

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Salient';
+export const SITE_TITLE = 'Salient Community';
 export const SITE_DESCRIPTION = 'Building what needs to be built. A community at the intersection of philosophy, values, and action-driven technology.';
